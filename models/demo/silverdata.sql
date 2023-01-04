050ef8bd86b6cc2bfd81235530d6a45dff68f360
maheshkolipaka992@gmail.com <maheshkolipaka992@gmail.com>
2023-01-04 09:57:31 +0000

    silverdata

---
+++ models/demo/silverdata.sql
@@ -0,0 +1,36 @@
+{{ config( materialized = 'table' ) }}
+
+
+with pos
+as
+(  
+   --Handling duplicate values  using QUALIFY clause and ROW_NUMBER() function
+     select * from airbyte_load.TRANSANDPOS
+      qualify row_number() over(partition by recordnumber order by recordnumber)=1 order by RECORDNUMBER
+
+)
+,transformed as(
+
+    --converting data type from string to date on tranformed data
+    select  RecordNumber , ClientID,ProductType, AgreementID, Value,
+     currency, to_date(AsOFDate,'dd-MM-yyyy') as AsOFDate, Collateralized, 
+     Counterparty, CounterpartyID, TypeofQFC,_airbyte_normalized_at as CreatedTime from pos
+)
+,final as(
+
+    --Adding Audit Columns(TransID,RecID,Createdby and Created time,Updated by and Updated time)
+    select *, '{{invocation_id}}' as TransID, 
+    row_number() over(partition by TransID order by RecordNumber) as RecID,
+    current_user() as CreatedBy,current_user() as UpdatedBy,
+    to_timestamp_tz(current_timestamp()) as UpdatedTime from transformed
+)
+select * from final 
+where  recordnumber is not null  
+and clientid is not null
+and ( producttype != '' and producttype is not null)
+and (producttype not RLIKE '[^a-zA-Z0-9]' and typeofqfc not rlike '[^a-zA-Z0-9]')
+and ASOFDATE like '____-__-__%'
+
+
+
+
